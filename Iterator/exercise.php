09c7df54ea5233a25845207d7b3fdac615a82b98
Danil Fut <twist1996@gmail.com>
2018-10-11 13:30:46 +0300

    Complete iterator exercise

--- Iterator/exercise.php
+++ Iterator/exercise.php
@@ -1,30 +1,47 @@
 <?php
 /**
  * Iterator design pattern (Fibonacci numbers)
- * 
+ *
  * @see    http://www.php.net/manual/en/class.iterator.php
  * @see    http://en.wikipedia.org/wiki/Fibonacci_number
  */
 
-class Fibonacci implements Iterator {
+class Fibonacci implements Iterator
+{
     protected $value = 0;
     protected $sum = 0;
     protected $key = 0;
-    public function rewind() {
+
+    public function rewind()
+    {
         $this->value = 0;
-        $this->key   = 0;
+        $this->key = 0;
     }
-    public function current() {
+
+    public function current()
+    {
         return $this->value;
     }
-    public function key() {
+
+    public function key()
+    {
         return $this->key;
     }
-    public function next() {
-        // @todo here the code to implement
+
+    public function next()
+    {
+        if ($this->value === 0) {
+            $this->value = 1;
+        } else {
+            $old = $this->value;
+            $this->value += $this->sum;
+            $this->sum = $old;
+        }
+        $this->key++;
     }
-    
-    public function valid() {
+
+    public function valid()
+    {
         return true;
     }
 }
@@ -33,6 +50,6 @@ class Fibonacci implements Iterator {
 $num = new Fibonacci();
 $correct = array(0, 1, 1, 2, 3, 5, 8, 13, 21, 34);
 for ($i = 0; $i < 10; $i++) {
-    printf ("%s<br>", $num->current() === $correct[$i] ? 'OK ' : 'ERROR');
+    printf("%s<br>", $num->current() === $correct[$i] ? 'OK ' : 'ERROR');
     $num->next();
 }
